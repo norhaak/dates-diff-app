699f54e0959b909991eb5050fc3be55db235da58
Mounir BARBISS <barbiss@gmail.com>
2019-01-19 00:03:27 +0000

    Added CSS styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleSubmit = event => {
+    event.preventDefault()
+      let startDate = event.target.elements.startDate.value
+      let endDate = event.target.elements.endDate.value
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Dates Diff App</h1>
+        <form className="App-form" onSubmit={this.handleSubmit}>
+          <label>
+            Start Date:
+            <input type="text" name="startDate" />
+          </label>
+          <label>
+            End Date:
+            <input type="text" name="endDate" />
+          </label>
+          <input type="submit" value="Submit"></input>
+        </form>
+       {/*  <div className="App-result">
+          There are days between {startDate} and {endDate}.
+        </div> */}
       </div>
     );
   }
